65a1392bc9e09a15561aa00dbc3addddcf36ccab
adhirajsingh1 <dhuruv.18@gmail.com>
2026-03-12 03:04:34 +0530

    terraform main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,38 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# ECS Cluster
+# ECS Cluster for running containers
 resource "aws_ecs_cluster" "skynet_cluster" {
   name = "skynet-audit-cluster"
 }
 
-# ECR Repository for Docker Image
-resource "aws_ecr_repository" "audit_repo" {
+# ECR repository to store Docker image
+resource "aws_ecr_repository" "audit_service_repo" {
   name = "skynet-audit-service"
 }
 
-# CloudWatch Log Group
+# CloudWatch log group for container logs
 resource "aws_cloudwatch_log_group" "audit_logs" {
   name              = "/ecs/skynet-audit-service"
   retention_in_days = 7
-}
-
-# Security Group
-resource "aws_security_group" "audit_service_sg" {
-  name        = "audit-service-sg"
-  description = "Allow traffic to audit service"
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
